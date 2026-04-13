84be88d6d3cef0350428a59b60dc9d0dda36bba6
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-13 13:22:38 -0400

    Getting all variables

--- src/main/java/com/plurasight/FullNameApplication.java
+++ src/main/java/com/plurasight/FullNameApplication.java
@@ -1,4 +1,20 @@
 package com.plurasight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner (System.in);
+        System.out.println("Please enter your name");
+        System.out.println("First Name: ");
+        String firstName = myScanner.nextLine();
+        System.out.println("Middle Name: ");
+        String middleName = myScanner.nextLine();
+        System.out.println("Last Name: ");
+        String lastName = myScanner.nextLine();
+        System.out.println("Suffix: ");
+        String suffix = myScanner.nextLine();
+
+
+    }
 }
