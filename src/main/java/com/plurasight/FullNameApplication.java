14ec629950baf197d43bc4c6d1edb54b0054e14f
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-13 14:48:45 -0400

    editing format

--- src/main/java/com/plurasight/FullNameApplication.java
+++ src/main/java/com/plurasight/FullNameApplication.java
@@ -1,20 +1,43 @@
 package com.plurasight;
 
+
 import java.util.Scanner;
 
 public class FullNameApplication {
     public static void main(String[] args) {
-        Scanner myScanner = new Scanner (System.in);
+        Scanner myScanner = new Scanner(System.in);
         System.out.println("Please enter your name");
-        System.out.println("First Name: ");
+        System.out.print("First Name: ");
         String firstName = myScanner.nextLine();
-        System.out.println("Middle Name: ");
+        String finalFirstName = firstName.trim().substring(0, 1).toUpperCase() + firstName.substring(1);
+
+        System.out.print("Middle Name: ");
         String middleName = myScanner.nextLine();
-        System.out.println("Last Name: ");
+        String finalMiddleName = middleName.trim();
+
+
+        System.out.print("Last Name: ");
         String lastName = myScanner.nextLine();
-        System.out.println("Suffix: ");
+        String finalLastName = lastName.trim().substring(0, 1).toUpperCase() + lastName.substring(1);
+
+
+        System.out.print("Suffix: ");
         String suffix = myScanner.nextLine();
+        String finalSuffix = suffix.trim();
+
+
+        String fullName = finalFirstName + " " + finalLastName;
+
+        if (!finalMiddleName.isEmpty()) {
+            fullName = finalFirstName + " " +finalMiddleName.substring(0, 1).toUpperCase() +". " + finalLastName + ", " + finalSuffix;
+            if (finalSuffix.isEmpty()) {
+                fullName = finalFirstName + " " + finalLastName;
+            }
+        }
+        System.out.println(fullName);
+        myScanner.close();
+
+    }
 
 
     }
-}
